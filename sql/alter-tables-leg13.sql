a71fd30d72f165e54c701661ee5f2e3d904c0cb2
RouxRC <b.ooghe@gmail.com>
2018-01-30 02:44:13 +0100

    alter existing nd tables

---
+++ sql/alter-tables-leg13.sql
@@ -0,0 +1,6 @@
+ALTER TABLE amendement               ADD auteur_groupe_acronyme        VARCHAR(16) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL AFTER date;
+ALTER TABLE parlementaire_amendement ADD parlementaire_groupe_acronyme VARCHAR(16) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL AFTER parlementaire_id;
+ALTER TABLE parlementaire_texteloi   ADD parlementaire_groupe_acronyme VARCHAR(16) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL AFTER parlementaire_id;
+ALTER TABLE presence                 ADD parlementaire_groupe_acronyme VARCHAR(16) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL AFTER parlementaire_id;
+ALTER TABLE intervention             ADD parlementaire_groupe_acronyme VARCHAR(16) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL AFTER parlementaire_id;
+ALTER TABLE question_ecrite          ADD parlementaire_groupe_acronyme VARCHAR(16) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL AFTER parlementaire_id;
